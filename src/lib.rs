36ce8131b89d8e1d5e1ad4e13f91ddeca023c48a
Jean-Marie Comets <jean.marie.comets@gmail.com>
2017-06-01 13:26:22 +0200

    Rewrite algorithm for correctness
    The following test cases failed, but now pass:
    - "" => "0000"
    - "TOTO" => "T300"
    - "ASHCROFT" => "A261"
    
    The algorithm abuses iterators and the itertools[1] crate, and returns an
    ASCII-only string using `String::from_utf8_unchecked`, which eliminates
    the need for any allocations.
    
    [1]: https://github.com/bluss/rust-itertools
    
    Note: as this is a full rewrite, I updated the crate authors as well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,17 @@
-// upper - helper function to uppercase chars
-fn upper(c: char) -> char {
-    match c.to_uppercase().next() {
-        Some(u) => u,
-        None=> ' '
-    }
-}
+extern crate itertools;
 
-// american_soundex_code - helper function that responds with the code based on the char
-fn soundex_code(c: char) -> char {
-    // soundex processing, replacements of letters
-    match upper(c) {
-        'B' | 'F' | 'P' | 'V' => {
-            // replace with 1
-            return '1';
-        },
-        'C' | 'G' | 'J' | 'K' | 'Q' | 'S' | 'X' | 'Z' => {
-            // replace with 2 
-            return '2';
-        },
-        'D' | 'T' => {
-            // replace with 3
-            return '3';
-        },
-        'L' => {
-            // replace with 4
-            return '4';
-        },
-        'M' | 'N' => {
-            // replace with 5
-            return '5';
-        },
-        'R' => {
-            // replace with 6
-            return '6';
-        },
-         'A' | 'E' | 'I' | 'O' | 'U' | 'Y' | 'H' | 'W' => {
-            // drop these, they aren't used for soundex
-            return ' ';
-        },
-        _ => {
-            // default to add a zero
-            return '0';
-        }
+use itertools::Itertools;
+use std::ascii::AsciiExt;
+
+fn american_soundex_code(c: u8) -> Option<u8> {
+    match c {
+        b'B' | b'F' | b'P' | b'V'                             => Some(b'1'),
+        b'C' | b'G' | b'J' | b'K' | b'Q' | b'S' | b'X' | b'Z' => Some(b'2'),
+        b'D' | b'T'                                           => Some(b'3'),
+        b'L'                                                  => Some(b'4'),
+        b'M' | b'N'                                           => Some(b'5'),
+        b'R'                                                  => Some(b'6'),
+        _                                                     => None,
     }
 }
 
@@ -52,95 +22,75 @@ fn soundex_code(c: char) -> char {
 /// ```
 /// use soundex;
 /// let code: String = soundex::american_soundex("Sirname");
+/// assert_eq!(&code, "S655");
 /// ```
 pub fn american_soundex(s: &str) -> String {
-    // Define a result
-    let mut result = String::from("");
-    // if we need to skip, outline which element to skip
-    let mut skip_i: usize = 0;
-    let mut count:i32 = 0;
-    let w_code = soundex_code('w');
-    let h_code = soundex_code('h');
-    // iterate over chars enumerated
-    for (i, v) in s.chars().enumerate() {
-        if i != 0 && skip_i == i {
-            // if this isn't the first, and skip index == current index break
-            continue;
-        }
-        if count > 3 {
-            // if the lenght is > 3, done
-            break;
-        } else if count >= 0 {
-            // if we have more than 0 elements, perform the indexing
-            // if the next is a duplicate, just move to next
-            let mut next: char = ' ';
-            let mut next_next: char = ' ';
+    let chars = move || {
+        s.chars()
+            .filter(|c| c.is_ascii())
+            .filter_map(|c| c.to_uppercase().next())
+            .map(|c| c as u8)
+    };
 
-            // grab next element, and the element after the next
-            let next_opt = s.chars().nth(i+1);
-            let next_next_opt = s.chars().nth(i+2);
+    let mut codes: Vec<_> = chars()
 
-            if next_opt.is_some() {
-                // validate this contains something before trying to unwrap
-                next = upper(next_opt.unwrap());
-            }
-            if next_next_opt.is_some() {
-                // validate this contains something before trying to unwrap
-                next_next = upper(next_next_opt.unwrap());
-            }
+        // remove H and W from the tail
+        .take(1).chain(chars().skip(1).filter(|&c| c != b'H' && c != b'W'))
 
-            let code: char = soundex_code(v);
-            let next_code: char = soundex_code(next);
-            let next_next_code: char = soundex_code(next_next);
+        // get the codes for each character (or None)
+        .map(|c| (c, american_soundex_code(c)))
 
-            if i + 1 <= s.len() && next_code == h_code ||  next_code == w_code {
-                // if the next is an h or w, compare current to the letter after the h or w, for
-                // duplication
-                if i +2 <= s.len() && code == next_next_code {
-                    skip_i = i + 2;
-                }
-            }
-            // if there are two duplicates in a row, remove the duplicate
-            if i + 1 <= s.len() && code == next_code {
-                skip_i = i + 1;
-            }
+        // remove adjacent chars
+        .group_by(|&(_, code)| code).into_iter()
+            .filter_map(|(_, mut g)| g.next())
 
-            if count == 0 {
-                // this is the first char in the string, goes through
-                result.push(upper(v));
+        // remove entries without codes (except first character)
+        .enumerate().filter_map(|(i, (c, code))| {
+            if i == 0 {
+                Some(c)
             } else {
-                // push the code value to the result
-                if code == ' ' {
-                    continue;
-                }
-                result.push(code);
+                code
             }
-        }
-        // increment counter
-        count+=1;
+        })
+
+        // pad with trailing zeros
+        .pad_using(4, |_| b'0').take(4)
+        .collect();
+
+    // if the first code is a digit, replace with the first character
+    if b'1' <= codes[0] && codes[0] <= b'9' {
+        codes[0] = chars().next().unwrap();
     }
-    // we didn't have enough chars in the string to fill 
-    // so we will backfill the last positions with '0'
-    while count <= 3 {
-        result.push('0');
-        count+=1;
-    }
-    // return the result
-    return result
+
+    // unsafe is ok here since we've already checked that the string is ascii
+    debug_assert!(codes.iter().all(|c| c.is_ascii()));
+    unsafe { String::from_utf8_unchecked(codes) }
 }
 
-#[test]
-fn american_soundex_correct() {
-    let params: Vec<(&str, &str)> = vec![
-        ("Pfister","P236"),
-        ("husobee","H210"),
-        ("Tymczak","T522"),
-        ("Ashcraft","A261"),
-        ("Robert","R163"),
-        ("Rupert","R163"),
-        ("Rubin","R150"),
-    ];
-    for (i, o) in params {
-        assert_eq!(american_soundex(i), o.to_string());
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn american_soundex_correct() {
+        let params: Vec<(&str, &str)> = vec![
+            ("",           "0000"),
+            ("007bond",    "0153"),
+            ("Ashcraft",   "A261"),
+            ("Pfister",    "P236"),
+            ("Robert",     "R163"),
+            ("Rubin",      "R150"),
+            ("Rupert",     "R163"),
+            ("Toto",       "T300"),
+            ("Tymczak",    "T522"),
+            ("husobee",    "H210"),
+            ("touchstone", "T235"),
+            ("heart ❤", "H630"),
+        ];
+
+        for (i, o) in params {
+            assert_eq!(american_soundex(i), o.to_string());
+        }
     }
+
 }
